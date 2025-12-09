873b1d246565d50e884a88ac330bdf7024659451
wanqiang <wanqiang@shizhuang-inc.com>
2025-12-09 11:16:48 +0800

    Add trace module with config and access channel
    Initialize trace module with config and access channel classes

---
+++ src/pyrocketmq/trace/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Trace module for pyrocketmq."""
+
+from .config import AccessChannel, TraceConfig
+
+__all__ = [
+    "AccessChannel",
+    "TraceConfig",
+]
